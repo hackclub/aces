55bfc90fda7d9b42d86bd0dc0fdac28187f95bd5
sahana <shayaandoodekula950@gmail.com>
2025-10-18 14:28:25 -0400

    change to <Link> + add home page to navbar if not on home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,30 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+
 export default function Navbar() {
+	const isHome = useRouter().pathname === "/"
   return (
     <div className="absolute w-full z-100 bg-transparent px-8 py-8 text-white">
       <div className="container mx-auto">
         <div className="md:space-x-6 space-x-2 text-right">
-          <a href="#learn-more" className="md:text-xl text-md hover:text-gray-300">
+	        {!isHome &&
+		        (<Link href="/" className="md:text-xl text-md hover:text-gray-300">
+		          Home
+	          </Link>)}
+          <Link href="/#learn-more" className="md:text-xl text-md hover:text-gray-300">
             About
-          </a>
-          <a href="#requirements" className="md:text-xl text-md hover:text-gray-300">
+          </Link>
+          <Link href="/#requirements" className="md:text-xl text-md hover:text-gray-300">
             Requirements
-          </a>
-          <a href="#events" className="md:text-xl text-md hover:text-gray-300">
-            Examples
-          </a>
-          <a href="#faq" className="md:text-xl text-md hover:text-gray-300">
+          </Link>
+          <Link href="/gallery" className="md:text-xl text-md hover:text-gray-300">
+	          Gallery
+          </Link>
+          <Link href="/#faq" className="md:text-xl text-md hover:text-gray-300">
             FAQ
-          </a>
+          </Link>
         </div>
       </div>
     </div>
