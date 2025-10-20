99e28e095c8ef16182351342138fa18986ee7193
jeninh <jenin.cmhoc@gmail.com>
2025-10-19 20:23:30 -0400

    Modify footer text and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="w-full text-center bg-rose-800 p-5 text-white">
-      <p className="md:text-lg text-md">Made by Hack Club. Check out our <a className="text-rose-200" href="https://github.com/hackclub">Github</a></p>
+      <p className="md:text-lg text-md">Made with love by Hack Club. Check out our <a className="text-rose-200" href="https://github.com/hackclub/aces">GitHub</a>, and the <a className="text-rose-200" href="https://hackclub.com/slack">Hack Club Slack</a>.</p>
     </div>
   );
 }
