de8015d98b4e4e17ab6a5c09e738c18b9bba4989
sahana <shayaandoodekula950@gmail.com>
2025-10-21 15:07:26 -0400

    Refactor Navbar component for improved accessibility and structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,25 +5,36 @@ import { useRouter } from "next/router";
 export default function Navbar() {
 	const isHome = useRouter().pathname === "/"
   return (
-    <div className="absolute w-full z-[100] bg-transparent px-8 py-8 text-white">
-	    <div className="mx-auto md:space-x-6 space-x-2 text-right">
-		    {!isHome &&
-			    (<Link href="/" className="md:text-xl text-md hover:text-gray-300">
-				    Home
-			    </Link>)}
-		    <Link href="/#learn-more" className="md:text-xl text-md hover:text-gray-300">
-			    About
-		    </Link>
-		    <Link href="/#requirements" className="md:text-xl text-md hover:text-gray-300">
-			    Requirements
-		    </Link>
-		    <a href="/gallery" className="md:text-xl text-md hover:text-gray-300">
-			    Gallery
-		    </a>
-		    <Link href="/#faq" className="md:text-xl text-md hover:text-gray-300">
-			    FAQ
-		    </Link>
-	    </div>
-    </div>
+	  <nav className="absolute w-full z-[100] bg-transparent px-8 py-8 text-white" role="navigation" aria-label="Main navigation">
+		  <ul className="mx-auto md:space-x-6 space-x-2 text-right">
+			  {!isHome && (
+				  <li className="inline">
+					  <Link href="/" className="md:text-xl text-md hover:text-gray-300">
+						  Home
+					  </Link>
+				  </li>
+			  )}
+			  <li className="inline">
+				  <Link href="/#learn-more" className="md:text-xl text-md hover:text-gray-300">
+					  About
+				  </Link>
+			  </li>
+			  <li className="inline">
+				  <Link href="/#requirements" className="md:text-xl text-md hover:text-gray-300">
+					  Requirements
+				  </Link>
+			  </li>
+			  <li className="inline">
+				  <a href="/gallery" className="md:text-xl text-md hover:text-gray-300">
+					  Gallery
+				  </a>
+			  </li>
+			  <li className="inline">
+				  <Link href="/#faq" className="md:text-xl text-md hover:text-gray-300">
+					  FAQ
+				  </Link>
+			  </li>
+		  </ul>
+	  </nav>
   );
 }
