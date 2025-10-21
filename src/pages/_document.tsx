9f8978f9655c5395c34c14799d68e0afb7dc1529
sahana <shayaandoodekula950@gmail.com>
2025-10-21 15:23:54 -0400

    Add skip link for improved accessibility to main content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
 				<Meta/>
 			</Head>
 			<body>
+			<a className="absolute p-3 m-3 left-0 -translate-y-20 focus:translate-y-0" href="#main-content">Skip to Main
+				Content</a>
 			<Main />
 			<NextScript />
 			</body>
