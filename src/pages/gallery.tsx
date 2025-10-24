eb1f9204402f80dd6707f8d44ffbafdf3ccc1b15
sahana <shayaandoodekula950@gmail.com>
2025-10-24 12:39:32 -0400

    galelry

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -4,10 +4,21 @@ import { useEffect, useState } from "react";
 export default function Gallery() {
 	const [projects, setProjects] = useState<Project[]>([])
 	useEffect(() => {
-		fetch("/api/gallery") // TODO: REPLACE WITH REAL API LINK.
+		fetch("/api/gallery")
 			.then(res => res.json())
 			.then(data => setProjects(data))
 	}, [])
 
-	return <></>
+	return <main>
+		<div className="container h-screen flex flex-col justify-evenly md:p-0 p-5">
+			<h1 className={"text-white text-8xl text-center"}>Gallery</h1>
+			<div className={"flex flex-col flex-wrap justify-evenly align-center"}>
+				{projects.map((project, i) => (
+					<div key={i}>
+						<p className={"text-center"}>{project.name}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	</main>
 }
