b2dce4e7032bb0e6c3a33e8107b68b188ff1afe1
sahana <shayaandoodekula950@gmail.com>
2025-10-27 09:11:49 -0400

    Fetch latest commit information from GitHub API for improved accuracy

--- src/pages/api/git.tsx
+++ src/pages/api/git.tsx
@@ -1,23 +1,21 @@
 import childProcess from "node:child_process";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const branch = process.env.NODE_ENV === "production" ? "main" : "dev";
+
 let cache: {
 	hash: string,
 	message: string
 } | undefined
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
 	if (!cache) {
-		const hash = childProcess
-			.execSync("git rev-parse --short HEAD")
-			.toString()
-			.trim();
-
-		const message = childProcess
-			.execSync("git log -1 --pretty=%s")
-			.toString()
-			.trim();
-		cache = {hash, message};
+		const githubRes = await fetch(`https://api.github.com/repos/hackclub/aces/commits/${branch}`)
+    const json = await githubRes.json()
+		cache = {
+      hash: json.sha.substring(0,7),
+      message: json.commit.message
+    };
 	}
 	res.status(200).json(cache);
 }
