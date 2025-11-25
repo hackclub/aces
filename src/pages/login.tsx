c404dbe34495019e696eb55df7266c4665dfa7aa
sahana <shayaandoodekula950@gmail.com>
2025-11-24 22:52:33 -0500

    yes.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 
 export default function LoginPage() {
-  const [hideOtp, setHideOtp] = useState(false);
+  const [otpVisible, setOtpVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const valid = /^\w+@\w+\.\w{2,}$/.test(email);
+
+  function clicky(
+    email: string,
+    otp: string | undefined
+  ) {
+    if (!otpVisible) {
+      alert("yes, sending otp to " + email);
+      setOtpVisible(true);
+    } else {
+      // do the verification
+      alert("verifying otp " + otp + " for " + email);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-md">
         <h1 className="text-4xl font-bold mb-8">Login Page</h1>
         <div className={"flex flex-col"}>
-          <div>
-            <input type={"checkbox"} name={"hideOtp"} className={"mb-4 mr-1"} defaultChecked={hideOtp} onInput={() => {setHideOtp(!hideOtp);}}/>
-            kjkasjdka
-          </div>
-          <input type={"email"} name={"email"} placeholder={"orpheus@hb.com"} className={"mb-4 p-2 border border-gray-300 rounded-lg w-64"} hidden={hideOtp} />
-          <input type={"password"} name={"otp"} placeholder={"679123"} className={"mb-4 p-2 border border-gray-300 rounded-lg w-64"} hidden={!hideOtp} />
-          <input type={"submit"} className={"mb-4 p-2 border border-gray-300 rounded-lg w-64"} />
+          <input type={"email"}
+                 id={"email"}
+                 placeholder={"orpheus@aces.deck"}
+                 className={`mb-4 p-2 border border-gray-300 rounded-lg w-64 ${
+                   valid ? "border-green-500" : email !== "" ? "border-red-300" : "border-gray-300"
+                 }`}
+                 onInput={(e) => setEmail(e.currentTarget.value)}
+                 hidden={otpVisible} />
+          <input type={"password"} id={"otp"} placeholder={"679123"} className={"mb-4 p-2 border border-gray-300 rounded-lg w-64"} hidden={!otpVisible} />
+          <input type={"button"} className={"mb-4 p-2 border border-gray-300 rounded-lg w-64"} value={"Login"} onClick={() => {
+            const email = document.getElementById("email") as HTMLInputElement | null;
+            const otp = document.getElementById("otp") as HTMLInputElement | null;
+            if (otp === null || !valid) return;
+            clicky(email!.value, otp?.value);
+          }} />
         </div>
       </div>
     </div>
